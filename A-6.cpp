670e649542b171ea28fdc3430f5c4357d4321239
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2019-10-30 11:47:00 +0800

    Add files via upload

---
+++ A-6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char num1,num2,num3,num4,num5,num6;
+	cin >> num1 >> num2 >> num3;
+	
+	num4 = (int)num1;
+	num5 = (int)num2;
+	num6 = (int)num3;
+	
+	cout << num4+num5+num6 << endl;	
+}
