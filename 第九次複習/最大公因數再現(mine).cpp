a9dc167c1fe6e6be22acbe2271f809eeeb74c6af
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2019-11-20 09:20:55 +0800

    Update 最大公因數再現(mine).cpp

--- 第九次複習/最大公因數再現(mine).cpp
+++ 第九次複習/最大公因數再現(mine).cpp
@@ -1,3 +1,31 @@
+/*=====================================
+第二題-最大公因數再現
+
+題目：請撰寫一個遞迴函式int gcd(int n, int m)，該函式接兩正整數n、m，計算兩數最大公因數之值。
+
+如gcd(12, 18)則傳回6。請使用遞迴完成，請勿使用迴圈方式。
+
+※ 請配合main函式執行，main函式請勿修改
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int num_1, num_2;
+
+	for(int i=0; i<10; i++)
+	{
+		cin >> num_1 >> num_2;
+		cout << gcd(num_1, num_2) << endl;
+	}
+
+	return 0;
+}
+======================================*/
+
 #include <iostream>
 using namespace std;
 
