27fb2fe179dd8271c86c87e3fbb7003f91dc6fd3
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2019-11-20 09:22:28 +0800

    Update 次方再現.cpp

--- 第九次複習/次方再現.cpp
+++ 第九次複習/次方再現.cpp
@@ -1,3 +1,30 @@
+/*================================
+第四題-次方再現
+
+題目：請撰寫一個遞迴函式int power(int base, int exp)，
+
+該函式接兩正整數base、exp，計算次方之值。請使用遞迴完成，請勿使用迴圈方式。
+
+※ 請配合main函式執行，main函式請勿修改
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int base, exp;
+
+	for(int i=0; i<10; i++)
+	{
+		cin >> base >> exp;
+		cout << power(base, exp) << endl;
+	}
+
+	return 0;
+}
+================================*/
+
 #include <iostream>
 using namespace std;
 
