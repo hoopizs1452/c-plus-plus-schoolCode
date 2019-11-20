e815f541441fd64cc36fbc9db04940b5437f9557
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2019-11-20 09:15:43 +0800

    Update Factorial再現 (三元運算子).cpp

--- 第九次複習/Factorial再現 (三元運算子).cpp
+++ 第九次複習/Factorial再現 (三元運算子).cpp
@@ -1,3 +1,31 @@
+/*===================================
+第一題-Factorial再現
+
+題目：請撰寫一個遞迴函式int factorial(int n)，該函式接收一正整數n，計算並傳回n!之值
+
+如factorial(5)則傳回120。請使用遞迴完成，請勿使用迴圈方式。
+
+※ 請配合main函式執行，main函式請勿修改
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int number;
+	
+	for(int i=0; i<10; i++)
+	{
+		cin >> number;
+		cout << factorial(number)<<endl;
+	}
+
+	return 0;
+}
+====================================*/
+
 #include <iostream>
 using namespace std;
 int factorial(int);
