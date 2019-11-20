b9d39100527506a20b5372e2e01b66bf2aa819c6
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2019-11-20 09:10:57 +0800

    Update 任意字元正方形.cpp

--- 第九次複習/任意字元正方形.cpp
+++ 第九次複習/任意字元正方形.cpp
@@ -1,3 +1,36 @@
+/*====================================
+第六題-任意字元正方形
+
+題目：請寫出一個函式void square(int side, char fillCharacter)，該函式在螢幕的左邊用字元參數fillCharacter畫出一個實心正方形，
+
+其邊長是由整數參數side指定。例如，如果使用者輸入邊長5，而字元為’#’，則會印出以下圖形：
+
+#####
+#####
+#####
+#####
+#####
+
+※ 請配合main函式執行，main函式請勿修改
+
+#include <iostream>
+using namespace std;
+
+void square(int side, char fillCharacter)
+{
+    
+}
+
+int main()
+{
+	int side;
+	char fillCharacter;
+
+	cin >> side >> fillCharacter;
+	square(side, fillCharacter);
+}
+====================================*/
+
 #include <iostream>
 using namespace std;
 
