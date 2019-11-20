37d75666a57483dd8d6ed96167d4eb6812303811
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2019-11-20 09:17:40 +0800

    Update 完全平方數相加.cpp

--- 第九次複習/完全平方數相加.cpp
+++ 第九次複習/完全平方數相加.cpp
@@ -1,3 +1,31 @@
+/*==================================
+第五題-完全平方數相加
+
+題目：請撰寫一個遞迴函式int sumSquareNumber(int n)，該函式接一正整數n，
+
+計算完全平方數至第n項的總和，如 1+4+9+16+....+n*n 。請使用遞迴完成，請勿使用迴圈方式。
+
+※ 請配合main函式執行，main函式請勿修改
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int number;
+
+	for(int i=0; i<10; i++)
+	{
+		cin >> number;
+		cout << sumSquareNumber(number) << endl;
+	}
+
+	return 0;
+}
+===================================*/
+
 #include <iostream>
 using namespace std;
 
